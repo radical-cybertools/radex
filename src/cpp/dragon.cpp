5bc6b580cfb23df4511682064c96413a9b9159ee
Andrew Shao <andrew.shao@hpe.com>
2026-08-04 16:11:56 -0700

    Override wait_for_bytes for Dragon Client
    Dragon's DDict implements an efficient algorithm that waits for a
    key to exist that does not rely on polling. The changes here
    check to make sure the DDict was created with this option on and
    also plumbs in a pass through to get_bytes for the Dragon client.

--- src/cpp/dragon.cpp
+++ src/cpp/dragon.cpp
@@ -5,13 +5,27 @@
 #include <cstdlib>
 #include <memory>
 #include <stdexcept>
+#include <utility>
 
 #include "dragon/serializable.hpp"
 
 namespace {
 
-dragon::DDict<dragon::Serializable, dragon::Serializable>
-_ddict_from_radex_env() {
+using DDict = dragon::DDict<dragon::Serializable, dragon::Serializable>;
+
+DDict _validate_ddict(DDict ddict) {
+
+    if (!ddict.wait_for_keys()) {
+        throw std::runtime_error(
+            "The DDict was not created with `wait_for_keys` enabled. "
+            "Check that the correct serialized DDict was passed to this "
+            "application."
+        );
+    }
+    return ddict;
+}
+
+DDict _ddict_from_radex_env() {
 
     const char *serialized_ddict = getenv(RADEX_STORE_VAR.c_str());
     if (!serialized_ddict) {
@@ -25,8 +39,15 @@ _ddict_from_radex_env() {
     }
     timespec_t timeout{timeout_seconds, 0};
 
-    return dragon::DDict<dragon::Serializable, dragon::Serializable>{
-        serialized_ddict, &timeout};
+    return DDict{serialized_ddict, &timeout};
+}
+
+DDict _ddict_from_descriptor(const char *descriptor, const timespec_t *timeout) {
+    if (!descriptor) {
+        throw std::invalid_argument("DDict descriptor must be specified.");
+    }
+
+    return DDict{descriptor, timeout};
 }
 
 } // namespace
@@ -36,10 +57,10 @@ namespace radex::drg::ddict {
 Client::Client() : Client(_ddict_from_radex_env()) {}
 
 Client::Client(dragon::DDict<dragon::Serializable, dragon::Serializable> ddict)
-    : ddict{ddict} {}
+    : ddict{_validate_ddict(std::move(ddict))} {}
 
 Client::Client(const char *descriptor, const timespec_t *timeout)
-    : ddict{descriptor, timeout} {}
+    : Client(_ddict_from_descriptor(descriptor, timeout)) {}
 
 bool Client::contains(std::string_view key) {
     dragon::SerializableString key_(std::string{key});
@@ -55,6 +76,20 @@ void Client::put_bytes(std::string_view key, const void *bytes,
     SerializableByteBuffer buf{length, ptr};
 
     ddict[key_] = buf;
+}
+
+detail::BytesBuffer Client::wait_for_bytes(std::string_view key,
+                                           std::chrono::milliseconds timeout) {
+    // TODO: Implement this when Dragon supports command-level timeout
+    if(!dormant_timeout_warning_triggered) {
+        std::cerr
+            << "The timeout for individual get commands has not been implemented. "
+            "Using the stored value from initialization"
+            << std::endl;
+
+        dormant_timeout_warning_triggered = true;
+    }
+    return get_bytes(key);
 }
 
 radex::detail::BytesBuffer Client::get_bytes(std::string_view key) {
