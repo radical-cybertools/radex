4eb34dc55379d4231838cd987722810b05223cf8
Andrew Shao <andrew.shao@hpe.com>
2026-08-10 15:02:51 -0700

    Rewrite literal as constructed due to header change

--- tests/python/dragon/test_py_cpp_interop.py
+++ tests/python/dragon/test_py_cpp_interop.py
@@ -195,7 +195,7 @@ def test_wait_for_scalars(
             client.put_scalar(radex::data::OutgoingHandle("{cpp_key}"), value);
 
             auto x = client.wait_for_scalar<{cpp_type_name}>(
-                radex::data::IncomingHandle("{py_key}"), 10'000ms);
+                radex::data::IncomingHandle("{py_key}"), std::chrono::milliseconds({10000}));
             return x == 12 ? 0 : 1;
         }}
         """))
@@ -273,7 +273,7 @@ def test_wait_for_tensors(
                 cpp_tensor);
 
             auto [dims, data] = client.wait_for_tensor<{cpp_type_name}>(
-                radex::data::IncomingHandle("{py_key}"), 10'000ms);
+                radex::data::IncomingHandle("{py_key}"), std::chrono:milliseconds({10000}));
             if (dims != expected_py_dims)
                 throw std::logic_error("Dims did not match");
             if (data != expected_py_data)
