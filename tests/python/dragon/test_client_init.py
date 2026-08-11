a6e394507d1e9ea2c350aceaf295707a98a35add
Matt Drozt <drozt@hpe.com>
2026-08-11 16:37:42 -0500

    Update test import path

--- tests/python/dragon/test_client_init.py
+++ tests/python/dragon/test_client_init.py
@@ -1,10 +1,10 @@
 import pytest
 from dragon.data.ddict import DDict
 
-import radex
+import radex.clients.core
 
 
 def test_init_raises_if_wait_for_keys_is_disabled(_requires_dragon_runtime):
     ddict = DDict(managers_per_node=1, n_nodes=1, wait_for_keys=False)
     with pytest.raises(RuntimeError, match=r"wait_for_keys"):
-        radex.DragonClient(descriptor=ddict.serialize(), timeout=1)
+        radex.clients.core.DragonClient(descriptor=ddict.serialize(), timeout=1)
