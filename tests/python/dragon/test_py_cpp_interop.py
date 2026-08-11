5e32aafa457c3100097efaccff16e2b3670845f6
Andrew Shao <andrew.shao@hpe.com>
2026-08-11 16:04:20 -0700

    stringify debugging

--- tests/python/dragon/test_py_cpp_interop.py
+++ tests/python/dragon/test_py_cpp_interop.py
@@ -184,16 +184,22 @@ def test_wait_for_scalars(
     bin_ = cpp_dragon_compile(textwrap.dedent(f"""\
         #include "radex/dragon.hpp"
         #include "radex/handles.hpp"
+        #include <iostream>
         #include <chrono>
+        #include <string>
         #include <thread>
 
         int main(void) {{
             timespec timeout {{5, 0}};
-            radex::drg::ddict::Client client {{"{ddict.serialize()}", &timeout}};
+
+
+            std::string ddict_serialize{{"{ddict.serialize()}"}};
+            radex::drg::ddict::Client client {{ddict_serialize.c_str(), &timeout}};
             {cpp_type_name} value = 123;
             std::this_thread::sleep_for(std::chrono::seconds({cpp_put_delay}));
             client.put_scalar(radex::data::OutgoingHandle("{cpp_key}"), value);
 
+            std::string py_key{{"{py_key}"}};
             auto x = client.wait_for_scalar<{cpp_type_name}>(
                 radex::data::IncomingHandle("{py_key}"), std::chrono::milliseconds({10000}));
             return x == 12 ? 0 : 1;
@@ -216,7 +222,7 @@ def test_wait_for_scalars(
     finally:
         proc.wait()
 
-    wait_interval = 2.0
+    wait_interval = 0.5
     assert cpp_put_delay - wait_interval < py_wait_time < cpp_put_delay + wait_interval
     assert value.dtype == np_dtype
     assert value == np_dtype(123)
@@ -252,6 +258,7 @@ def test_wait_for_tensors(
         #include "radex/handles.hpp"
         #include <chrono>
         #include <numeric>
+        #include <string>
         #include <thread>
         #include <vector>
 
@@ -297,7 +304,7 @@ def test_wait_for_tensors(
         proc.kill()
     finally:
         proc.wait()
-    wait_interval = 2.0
+    wait_interval = 0.5
     assert cpp_put_delay - wait_interval < py_wait_time < cpp_put_delay + wait_interval
     assert cpp_tensor.dtype == expected_cpp_tensor.dtype == np_dtype
     assert (cpp_tensor == expected_cpp_tensor).all()
