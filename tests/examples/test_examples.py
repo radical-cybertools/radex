a67b14641791e96fc280cd754794232627a1f521
Matt Drozt <drozt@hpe.com>
2026-08-18 14:36:51 -0500

    Remove filecmp for more fine grain error from test suite

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -1,7 +1,10 @@
-import filecmp
-
-
 def test_run_example(example, tmp_path):
     returncode, out, err = example.run(where=tmp_path)
     assert returncode == 0
-    assert filecmp.cmp(out, example.expected_stdout)
+
+    with (
+        open(out, "r", encoding="utf-8") as fh,
+        open(example.expected_stdout, "r", encoding="utf-8") as xfh,
+    ):
+        for line, xline in zip(fh, xfh):
+            assert line == xline
