61207f58dc7ec435b98331260570e8e42fea8122
Matt Drozt <drozt@hpe.com>
2026-08-18 14:57:19 -0500

    Debug CI

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -2,9 +2,19 @@ def test_run_example(example, tmp_path):
     returncode, out, err = example.run(where=tmp_path)
     assert returncode == 0
 
+    print("Expected:")
+    print("---------")
+    with open(example.expected_stdout, "r", encoding="utf-8") as f:
+        print(f.read())
+
+    print("\nGot:")
+    print("----")
+    with open(out, "r", encoding="utf-8") as f:
+        print(f.read())
+
     with (
         open(out, "r", encoding="utf-8") as fh,
         open(example.expected_stdout, "r", encoding="utf-8") as xfh,
     ):
         for line, xline in zip(fh, xfh):
-            assert line == xline
+            assert line.strip() == xline.strip()
