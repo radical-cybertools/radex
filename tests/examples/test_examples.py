8ce52101f8c709c4a60063e12d971d812b2ce207
Matt Drozt <drozt@hpe.com>
2026-08-19 13:36:34 -0500

    remove debug step from test

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -1,22 +1,6 @@
 def test_run_example(example, tmp_path):
     returncode, out, err = example.run(where=tmp_path)
     assert returncode == 0
-
-    print("Expected:")
-    print("---------")
-    with open(example.expected_stdout, "r", encoding="utf-8") as f:
-        print(f.read())
-
-    print("\nGot:")
-    print("----")
-    with open(out, "r", encoding="utf-8") as f:
-        print(f.read())
-
-    print("\nError:")
-    print("------")
-    with open(err, "r", encoding="utf-8") as f:
-        print(f.read())
-
     with (
         open(out, "r", encoding="utf-8") as fh,
         open(example.expected_stdout, "r", encoding="utf-8") as xfh,
