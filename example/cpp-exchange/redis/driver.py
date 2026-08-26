87cfff7b87f3c400bab87b0389881211801ad520
Matt Drozt <matthew.drozt@gmail.com>
2026-08-25 18:44:04 -0700

    Adds SmartRedis example to the test suite. (#41)
    Adds the `example/cpp-exchange/redis` example to the test suite.
    
    However, this example will be skipped in the CI unless/until we decide to install SmartSim and SmartRedis prior to running the test suite. This is simply a convenience for people who have the example set up to check that the output matches what is expected through the test suite.

--- example/cpp-exchange/redis/driver.py
+++ example/cpp-exchange/redis/driver.py
@@ -1,23 +1,37 @@
 import os
 import pathlib
+import shutil
+import textwrap
 import time
 
 from smartsim import Experiment
 
-HERE = pathlib.Path(__file__).parent.absolute()
+HERE = pathlib.Path(__file__).resolve().parent
 ROOT = HERE.parent.parent.parent
-BUILD = ROOT / "build"
+EXAMPLES_BIN_DIR = ROOT / "install" / "bin" / "examples"
 
 
 def main() -> int:
-    exp = Experiment("cpp-sr-client-exchange")
+    exp_name = "cpp-sr-client-exchange"
+    producer_name = "producer"
+    consumer_name = "consumer"
 
-    db = exp.create_database(db_nodes=1, interface="hsn0")
+    os.environ["SR_LOG_LEVEL"] = "QUIET"
+    if (HERE / exp_name).exists():
+        shutil.rmtree(HERE / exp_name)
 
-    producer_settings = exp.create_run_settings(os.fspath(BUILD / "redis-cpp-producer"))
+    exp = Experiment(exp_name, launcher="local")
+
+    db = exp.create_database(db_nodes=1, interface="lo")
+
+    producer_settings = exp.create_run_settings(
+        os.fspath(EXAMPLES_BIN_DIR / "redis-cpp-producer")
+    )
     producer = exp.create_model("producer", producer_settings)
 
-    consumer_settings = exp.create_run_settings(os.fspath(BUILD / "redis-cpp-consumer"))
+    consumer_settings = exp.create_run_settings(
+        os.fspath(EXAMPLES_BIN_DIR / "redis-cpp-consumer")
+    )
     consumer = exp.create_model("consumer", consumer_settings)
 
     exp.generate(db, producer, consumer)
@@ -25,10 +39,24 @@ def main() -> int:
     time.sleep(3)
 
     try:
-        exp.start(producer, block=True)
-        exp.start(consumer, block=True)
+        print("SmartSim is running the producer")
+        exp.start(producer, block=True, monitor=False)
+        print("SmartSim is running the consumer")
+        exp.start(consumer, block=True, monitor=False)
     finally:
         exp.stop(db)
+
+    print("PRODUCER OUTPUT:")
+    with open(
+        HERE / exp_name / producer_name / f"{producer_name}.out", "r", encoding="utf-8"
+    ) as f:
+        print(textwrap.indent(f.read(), "    "))
+
+    print("CONSUMER OUTOUT:")
+    with open(
+        HERE / exp_name / consumer_name / f"{consumer_name}.out", "r", encoding="utf-8"
+    ) as f:
+        print(textwrap.indent(f.read(), "    "))
 
     return 0
 
